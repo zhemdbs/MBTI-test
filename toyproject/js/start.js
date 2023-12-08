d350177511749c1eccf69ab087cc00cfa606877a
zhemdbs <zhemdbs@gmail.com>
2023-12-08 16:57:19 +0900

    💄 QnA Page - 진행 상태 바 구현

--- toyproject/js/start.js
+++ toyproject/js/start.js
@@ -1,6 +1,7 @@
 const main = document.querySelector('#main');
 const qna = document.querySelector('#qna');
 const startBtn = document.querySelector('.btn__start');
+const endPoint = 12;
 
 startBtn.addEventListener('click', () => {
   begin();
@@ -10,25 +11,37 @@ function addAnswer(answerText, qIdx) {
   var a = document.querySelector('.answerBox');
   var answer = document.createElement('button');
   answer.classList.add('answerList');
+  answer.classList.add('fadeIn');
+
   a.appendChild(answer);
   answer.innerHTML = answerText;
+
   answer.addEventListener('click', () => {
     var children = document.querySelectorAll('.answerList');
     for (let i = 0; i < children.length; i++) {
       children[i].disabled = true;
-      children[i].style.display = 'none';
+      children[i].style.webkitAnimation = 'fadeOut 0.5s';
+      children[i].style.animation = 'fadeOut 0.5s';
     }
-    goNext(++qIdx);
+    setTimeout(() => {
+      for (let i = 0; i < children.length; i++) {
+        children[i].style.display = 'none';
+      }
+      goNext(++qIdx);
+    }, 450);
   });
 }
 
 function goNext(qIdx) {
+  var status = document.querySelector('.statusBar');
   var q = document.querySelector('.qBox');
   q.innerHTML = qnaList[qIdx].q;
 
   for (let i in qnaList[qIdx].a) {
     addAnswer(qnaList[qIdx].a[i].answer, qIdx);
   }
+
+  status.style.width = (100 / endPoint) * (qIdx + 1) + '%';
 }
 
 function begin() {
